be0c255a4f6da796f3bad59ad44c4fa8959b41df
vitalii-samoilenko <vitalii_samoilenko@hotmail.com>
2025-06-28 23:31:35 +0300

    json config

--- src/hosts/console/main.cpp
+++ src/hosts/console/main.cpp
@@ -1,8 +1,10 @@
 #include <chrono>
-#include <exception>
-#include <future>
+#include <stdexcept>
+#include <fstream>
 #include <iostream>
 #include <string>
+
+#include "boost/json.hpp"
 
 #include "ai/chat/clients/twitch/auth.hpp"
 #include "ai/chat/clients/twitch/irc.hpp"
@@ -56,34 +58,71 @@ void init_logger() {
     ::std::shared_ptr<::opentelemetry::logs::LoggerProvider> api_provider{ ::std::move(sdk_provider) };
     ::opentelemetry::logs::Provider::SetLoggerProvider(api_provider);
 };
+::std::string auth_address{};
+::std::chrono::milliseconds auth_timeout{};
+::std::string auth_client_id{};
+::std::string auth_client_secret{};
+::std::string auth_refresh_token{};
+::std::string irc_address{};
+::std::chrono::milliseconds irc_timeout{};
+::std::string irc_botname{};
+::std::string openai_address{};
+::std::chrono::milliseconds openai_timeout{};
+::std::string openai_model{};
+::std::string openai_key{};
+::std::string sqlite_filename{};
+void init_config(const ::std::string& filename) {
+    ::std::ifstream input{ filename };
+    if (!input.is_open()) {
+        throw ::std::invalid_argument{ "configuration not found" };
+    }
+    ::boost::json::monotonic_resource buffer{};
+    ::boost::json::stream_parser parser{ &buffer };
+    ::std::string line{};
+    while (::std::getline(input, line)) {
+        parser.write_some(line);
+    }
+    ::boost::json::value config{ parser.release() };
+    ::boost::json::value& client{ config.at("client") };
+    ::boost::json::value& auth{ client.at("auth") };
+    ::boost::json::value& irc{ client.at("irc") };
+    ::boost::json::value& sqlite{ config.at("history") };
+    ::boost::json::value& openai{ config.at("adapter") };
+    auth_address.append(auth.at("address").as_string());
+    auth_timeout = ::std::chrono::milliseconds{ auth.at("timeout").as_int64() };
+    auth_client_id.append(auth.at("client_id").as_string());
+    auth_client_secret.append(auth.at("client_secret").as_string());
+    auth_refresh_token.append(auth.at("refresh_token").as_string());
+    irc_address.append(irc.at("address").as_string());
+    irc_timeout = ::std::chrono::milliseconds{ irc.at("timeout").as_int64() };
+    irc_botname.append(irc.at("botname").as_string());
+    openai_address.append(openai.at("address").as_string());
+    openai_timeout = ::std::chrono::milliseconds{ openai.at("timeout").as_int64() };
+    openai_model.append(openai.at("model").as_string());
+    openai_key.append(openai.at("key").as_string());
+    sqlite_filename.append(sqlite.at("filename").as_string());
+}
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
         init_meter();
         init_tracer();
         init_logger();
+        init_config(argc
+            ? argv[0]
+            : "config.json");
 
-        ::std::chrono::milliseconds timeout{ 30 * 1000 };
-        ::std::string auth_address{ "https://id.twitch.tv/oauth2/" };
-        ::std::string client_id{ "client_id" };
-        ::std::string client_secret{ "client_secret" };
-        ::std::string refresh_token{ "refresh_token" };
-        ::std::string irc_address{ "wss://irc-ws.chat.twitch.tv" };
-        ::std::string botname{ "botname" };
-        ::std::string openai_address{ "https://generativelanguage.googleapis.com/v1beta/openai/" };
-        ::std::string model{ "gemini-2.0-flash" };
-        ::std::string key{ "key" };
-
-        ::ai::chat::clients::twitch::auth auth{ auth_address, timeout };
-        ::ai::chat::clients::twitch::irc<::ai::chat::clients::twitch::handlers::observable> irc{ 0, irc_address, timeout };
-        ::ai::chat::adapters::openai openai{ openai_address, timeout };
-        ::ai::chat::histories::observable<::ai::chat::histories::sqlite> sqlite{ botname };
+        ::ai::chat::clients::twitch::auth auth{ auth_address, auth_timeout };
+        ::ai::chat::clients::twitch::irc<::ai::chat::clients::twitch::handlers::observable> irc{ 0, irc_address, irc_timeout };
+        ::ai::chat::adapters::openai openai{ openai_address, openai_timeout };
+        ::ai::chat::histories::observable<::ai::chat::histories::sqlite> sqlite{ sqlite_filename };
         auto irc_binding = ::ai::chat::binders::twitch<decltype(sqlite), decltype(irc)>::bind(sqlite, irc);
         auto openai_binding = ::ai::chat::binders::openai<decltype(sqlite), decltype(openai)>::bind(sqlite, openai,
-            botname, model, key);
+            irc_botname, openai_model, openai_key);
 
-        ::ai::chat::clients::twitch::token_context access_context{ auth.refresh_token(client_id, client_secret, refresh_token) };
-        irc.connect(botname, access_context.access_token);
+        ::ai::chat::clients::twitch::token_context access_context{ auth.refresh_token(auth_client_id, auth_client_secret, auth_refresh_token) };
+        irc.connect(irc_botname, access_context.access_token);
+        ::std::cout << "Waiting for shutdown signal..." << ::std::endl;
         irc.attach();
     }
     catch(const ::std::exception& e) {
